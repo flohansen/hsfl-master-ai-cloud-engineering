ad52dbe28e23c11530151521accab958556600d1
Fabian Petersen <fabian@nf-petersen.de>
2023-10-06 16:58:06 +0200

    feat(text-service): start implementing service

---
+++ src/text-service/go.mod
@@ -0,0 +1,9 @@
+module text-service
+
+go 1.21
+
+require (
+	github.com/akatranlp/hsfl-master-ai-cloud-engineering/http-router v0.0.0
+)
+
+replace github.com/akatranlp/hsfl-master-ai-cloud-engineering/http-router => ../http-router
---
+++ src/text-service/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/akatranlp/hsfl-master-ai-cloud-engineering/http-router"
+	"net/http"
+)
+
+func main() {
+	router := http_router.New()
+
+	router.GET("/books", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	router.GET("/books/:bookId", func(w http.ResponseWriter, r *http.Request) {
+		bookId := r.Context().Value("bookId").(string)
+	})
+
+	router.GET("/books/:bookId/chapters", func(w http.ResponseWriter, r *http.Request) {
+		bookId := r.Context().Value("bookId").(string)
+	})
+
+	router.GET("/books/:bookId/chapters/:chapterId", func(w http.ResponseWriter, r *http.Request) {
+		bookId := r.Context().Value("bookId").(string)
+		chapterId := r.Context().Value("chapterId").(string)
+	})
+
+	router.POST("/books", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	router.PUT("/books/:bookId", func(w http.ResponseWriter, r *http.Request) {
+		bookId := r.Context().Value("bookId").(string)
+	})
+
+	router.DELETE("/books/:bookId", func(w http.ResponseWriter, r *http.Request) {
+		bookId := r.Context().Value("bookId").(string)
+	})
+
+	router.POST("/books/:bookId/chapters", func(w http.ResponseWriter, r *http.Request) {
+		bookId := r.Context().Value("bookId").(string)
+	})
+
+	router.PUT("/books/:bookId/chapters/:chapterId", func(w http.ResponseWriter, r *http.Request) {
+		bookId := r.Context().Value("bookId").(string)
+		chapterId := r.Context().Value("chapterId").(string)
+	})
+
+	router.DELETE("/books/:bookId/chapters/:chapterId", func(w http.ResponseWriter, r *http.Request) {
+		bookId := r.Context().Value("bookId").(string)
+		chapterId := r.Context().Value("chapterId").(string)
+	})
+}
