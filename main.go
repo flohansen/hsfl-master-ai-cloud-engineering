e81b9af9992bd2485d76b5fed8096bb7a47422dc
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2023-10-06 17:10:30 +0200

    feat(web-service): add handler to main

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
 func main() {
+	handler := router.New()
 	
+	if err := http.ListenAndServe(":3000", handler); err != nil {
+		log.Fatalf("error while listen and serve: %s", err.Error())
+	}
+
 }
