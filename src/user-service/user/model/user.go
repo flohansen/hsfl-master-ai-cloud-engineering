c7bd425ae9eb5dcd7bdb39a808fb0c0fb040587b
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2023-10-12 12:38:06 +0200

    feat(user-service): define functions in user repo

--- src/user-service/user/model/user.go
+++ src/user-service/user/model/user.go
@@ -1,6 +1,7 @@
 package model
 
 type User struct {
+	Id       uint64
 	Email    string
 	Password []byte
 	Name     string
--- src/user-service/user/repository.go
+++ src/user-service/user/repository.go
@@ -1,4 +1,10 @@
 package user
 
+import "hsfl.de/group6/hsfl-master-ai-cloud-engineering/user-service/user/model"
+
 type Repository interface {
+	Create([]*model.User) error
+	Delete([]*model.User) error
+	FindById(id uint64) ([]*model.User, error)
+	Update([]*model.User) ([]*model.User, error)
 }
