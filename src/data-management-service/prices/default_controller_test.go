e1bc3feaa970adbb6f08de6a5ff93d2c114be3f3
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2023-10-16 17:13:08 +0200

    feat(data-management-service): add test for POST for prices in controller

--- src/data-management-service/prices/default_controller_test.go
+++ src/data-management-service/prices/default_controller_test.go
@@ -196,7 +196,110 @@ func TestDefaultController_GetPrice(t *testing.T) {
 	})
 }
 
-func TestDefaultController_PutProduct(t *testing.T) {
+func TestDefaultController_PostPrice(t *testing.T) {
+	type fields struct {
+		priceRepository Repository
+	}
+	type args struct {
+		writer  *httptest.ResponseRecorder
+		request *http.Request
+	}
+	tests := []struct {
+		name             string
+		fields           fields
+		args             args
+		expectedStatus   int
+		expectedResponse string
+	}{
+		{
+			name: "Valid Price",
+			fields: fields{
+				priceRepository: setupMockRepository(),
+			},
+			args: args{
+				writer: httptest.NewRecorder(),
+				request: httptest.NewRequest(
+					"POST",
+					"/api/v1/price",
+					strings.NewReader(`{"userId": 3, "productId": 3, "price": 0.99}`),
+				),
+			},
+			expectedStatus:   http.StatusOK,
+			expectedResponse: "",
+		},
+		{
+			name: "Valid Price (Partly Fields)",
+			fields: fields{
+				priceRepository: setupMockRepository(),
+			},
+			args: args{
+				writer: httptest.NewRecorder(),
+				request: httptest.NewRequest(
+					"POST",
+					"/api/v1/price",
+					strings.NewReader(`{"price": 7.10}`),
+				),
+			},
+			expectedStatus:   http.StatusOK,
+			expectedResponse: "",
+		},
+		{
+			name: "Malformed JSON",
+			fields: fields{
+				priceRepository: setupMockRepository(),
+			},
+			args: args{
+				writer: httptest.NewRecorder(),
+				request: httptest.NewRequest(
+					"POST",
+					"/api/v1/price",
+					strings.NewReader(`{"price": 7.10`),
+				),
+			},
+			expectedStatus:   http.StatusBadRequest,
+			expectedResponse: "",
+		},
+		{
+			name: "Invalid price, incorrect Type for price (Non-numeric)",
+			fields: fields{
+				priceRepository: setupMockRepository(),
+			},
+			args: args{
+				writer: httptest.NewRecorder(),
+				request: httptest.NewRequest(
+					"POST",
+					"/api/v1/product",
+					strings.NewReader(`{"userId": 4, "productId": 4, "price": "0.99"}`),
+				),
+			},
+			expectedStatus:   http.StatusBadRequest,
+			expectedResponse: "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			controller := defaultController{
+				priceRepository: tt.fields.priceRepository,
+			}
+			controller.PostPrice(tt.args.writer, tt.args.request)
+
+			// You can then assert the response status and content, and check against your expectations.
+			if tt.args.writer.Code != tt.expectedStatus {
+				t.Errorf("Expected status code %d, but got %d", tt.expectedStatus, tt.args.writer.Code)
+			}
+
+			if tt.expectedResponse != "" {
+				actualResponse := tt.args.writer.Body.String()
+				if actualResponse != tt.expectedResponse {
+					t.Errorf("Expected response: %s, but got: %s", tt.expectedResponse, actualResponse)
+				}
+			}
+		})
+	}
+}
+
+func TestDefaultController_PutPrice(t *testing.T) {
 	type fields struct {
 		priceRepository Repository
 	}
@@ -222,7 +325,7 @@ func TestDefaultController_PutProduct(t *testing.T) {
 				request: func() *http.Request {
 					var request = httptest.NewRequest(
 						"PUT",
-						"/api/v1/prices/1/1",
+						"/api/v1/price/1/1",
 						strings.NewReader(`{"userId": 1, "productId": 1, "price": 10.99}`))
 					request = request.WithContext(context.WithValue(request.Context(), "productId", "1"))
 					request = request.WithContext(context.WithValue(request.Context(), "userId", "1"))
@@ -242,7 +345,7 @@ func TestDefaultController_PutProduct(t *testing.T) {
 				request: func() *http.Request {
 					var request = httptest.NewRequest(
 						"PUT",
-						"/api/v1/prices/2/2",
+						"/api/v1/price/2/2",
 						strings.NewReader(`{"price": 6.50}`))
 					request = request.WithContext(context.WithValue(request.Context(), "productId", "2"))
 					request = request.WithContext(context.WithValue(request.Context(), "userId", "2"))
@@ -262,7 +365,7 @@ func TestDefaultController_PutProduct(t *testing.T) {
 				request: func() *http.Request {
 					var request = httptest.NewRequest(
 						"PUT",
-						"/api/v1/prices/2/2",
+						"/api/v1/price/2/2",
 						strings.NewReader(`{"price": 6.50`))
 					request = request.WithContext(context.WithValue(request.Context(), "productId", "2"))
 					request = request.WithContext(context.WithValue(request.Context(), "userId", "2"))
@@ -282,7 +385,7 @@ func TestDefaultController_PutProduct(t *testing.T) {
 				request: func() *http.Request {
 					var request = httptest.NewRequest(
 						"PUT",
-						"/api/v1/prices/2/2",
+						"/api/v1/price/2/2",
 						strings.NewReader(`{"price": "Wrong Type"`))
 					request = request.WithContext(context.WithValue(request.Context(), "productId", "2"))
 					request = request.WithContext(context.WithValue(request.Context(), "userId", "2"))
