bb7f181fc230035435533915991d28ba3cf895d9
Jannick <jannick.lindner@hotmail.de>
2023-10-26 13:58:22 +0200

    feat(data-management-service): update models for List and Entry

--- src/data-management-service/userShoppingList/model/userShoppingList.go
+++ src/data-management-service/userShoppingList/model/userShoppingList.go
@@ -1,6 +1,7 @@
 package model
 
 type UserShoppingList struct {
-	Id     uint64 `json:"Id,omitempty"`
-	UserId uint64 `json:"userId,omitempty"`
+	Id        uint64 `json:"Id,omitempty"`
+	UserId    uint64 `json:"userId,omitempty"`
+	Completed bool   `json:"completed,omitempty"`
 }
