9ba98857a98e2d8e839c4271c9efe8981dc79702
jarali <jannick.lindner@hotmail.de>
2023-10-31 10:54:31 +0100

    refactor(data-management-service): delete unused folder

--- src/data-management-service/userShoppingList/model/userShoppingList.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type UserShoppingList struct {
-	Id        uint64 `json:"Id,omitempty"`
-	UserId    uint64 `json:"userId,omitempty"`
-	Completed bool   `json:"completed,omitempty"`
-}
