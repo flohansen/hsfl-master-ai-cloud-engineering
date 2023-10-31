b26a884e90d9bf650e8793873241734e6079daa9
Fabian Petersen <fabian@nf-petersen.de>
2023-10-31 16:18:45 +0100

    feat(web-service): Use React Query to get the first books

--- src/web-service/src/App.tsx
+++ src/web-service/src/App.tsx
@@ -1,11 +1,36 @@
-import {ModeToggle} from "@/components/mode-toggle.tsx";
+import { ModeToggle } from "@/components/mode-toggle.tsx";
+import {useQuery} from "@tanstack/react-query";
+import {Book, getAllBooks} from "@/repository/books.ts";
+
+const BookCard = ({book}: {book: Book}) => {
+    return <div>{book.description}</div>
+}
 
 function App() {
+    const {data, isError, isLoading, isSuccess, error} = useQuery({
+        queryKey: ["books"],
+        queryFn: getAllBooks
+    })
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+
+    if (isError) {
+        return <div>Error {error.message}</div>
+    }
+
+    if (!isSuccess) {
+        return <div>Something went wrong!</div>
+    }
+
   return (
     <div>
       <ModeToggle />
+        <div>{data.map(book => <BookCard book={book} />)}</div>
+        <div>Hello World!</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/web-service/src/repository/books.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export type Book = {
+    Id: number
+    description: string
+}
+
+export const getAllBooks = async () => {
+    const response = await axios.get("/api/v1/books");
+    return response.data as Book[];
+}
