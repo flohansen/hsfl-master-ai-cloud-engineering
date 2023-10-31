5534154eea9f9acaa52e9c9e7e57ac1751efcf1d
jarali <jannick.lindner@hotmail.de>
2023-10-31 10:54:31 +0100

    refactor(data-management-service): delete unused folder

--- src/data-management-service/userShoppingListEntry/model/userShoppingListEntry.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type UserShoppingListEntry struct {
-	ShoppingListId uint64 `json:"shoppingListId,omitempty"`
-	ProductId      uint64 `json:"productId,omitempty"`
-	Count          uint16 `json:"count,omitempty"`
-	Note           string `json:"note,omitempty"`
-	Checked        bool   `json:"checked,omitempty"`
-}
