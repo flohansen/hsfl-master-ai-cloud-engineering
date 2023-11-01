e130671d0cd8184af89204f356bd5fca626cc790
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2023-11-01 13:40:16 +0100

    feat(web-service): create entry point

---
+++ src/web-service/svelte/src/lib/navigation/NavBar.svelte
@@ -0,0 +1,12 @@
+<aside class="fixed bottom-0 bg-white w-full rounded-t-[2.5rem] px-5 py-4 drop-shadow-navBar">
+    <div class="flex items-center justify-between">
+        <NavItem icon="home"/>
+        <NavItem icon="list"/>
+        <NavItem icon="cart"/>
+        <NavItem icon="profile"/>
+    </div>
+</aside>
+
+<script lang="ts">
+    import NavItem from "./NavItem.svelte";
+</script>
