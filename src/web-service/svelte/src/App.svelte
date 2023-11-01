173647ecec6c9efc9ffee534105f67e9524a4f1e
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2023-11-01 16:34:24 +0100

    feat(web-service): create ShoppingListItem component

--- src/web-service/svelte/src/App.svelte
+++ src/web-service/svelte/src/App.svelte
@@ -1,27 +1,23 @@
 <header class="px-5 mt-16 flex items-center justify-between sm:ml-20 md:ml-24 lg:max-w-4xl lg:mx-auto xl:max-w-5xl">
-    <h1 class="font-bold text-lg lg:text-xl xl:text-3xl">PriceWhisper</h1>
+    <h1 class="font-bold text-xl md:text-2xl xl:text-3xl">
+        Deine Einkaufslisten
+    </h1>
 </header>
 
 <main class="sm:ml-20 md:ml-24 lg:max-w-4xl lg:mx-auto xl:max-w-5xl">
-    <section class="mx-5 mt-8 rounded-2xl bg-blue-light/50 p-5 lg:p-8">
-        <figure class="w-12 h-12 flex items-center justify-center rounded-full bg-blue-dark/40">
-            <Euro classes="w-7 h-7 text-blue-dark"></Euro>
-        </figure>
-        <h2 class="text-lg font-semibold mt-5 lg:text-xl xl:text-2xl xl:mt-8">
-            Spare bis zu 35% Prozent auf deinen nächsten Supermarkt-Einkauf!
-        </h2>
-        <p class="mt-3 text-base font-light lg:text-lg">
-            Tempor qui cillum fugiat aliqua Lorem sint laborum duis. Esse elit dolore dolor quis mollit.
-            Eu excepteur occaecat ea adipisicing ipsum proident nisi.
-        </p>
-    </section>
+    <h2 class="px-5 text-gray-dark text-sm font-medium mt-10 lg:text-base">
+        Offene Einkaufslisten
+    </h2>
+    <ul class="px-5 mt-4 grid grid-cols-1 gap-y-4 lg:gap-y-6 lg:mt-6">
+        <ShoppingListItem description="Geburtstagskuchen"/>
+        <ShoppingListItem description="Frühstück mit Anne"/>
+    </ul>
 </main>
 
 <NavBar/>
-
 <script lang="ts">
-    import Euro from "./assets/svg/Euro.svelte";
     import NavBar from "./lib/navigation/NavBar.svelte";
+    import ShoppingListItem from "./lib/ShoppingListItem.svelte";
 </script>
 
 
