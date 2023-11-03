a87adf802e562c3cdb2bf7550a3cd25cdb724b43
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2023-11-03 14:28:25 +0100

    chore(web-service): cleanup and update readme

--- src/web-service/frontend/src/lib/ShoppingListItem.svelte
+++ src/web-service/frontend/src/lib/ShoppingListItem.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+    export let description: string = 'Name der Einkaufsliste';
+    export let count: number = 0;
+</script>
+
 <li>
     <a href="/" class="bg-white w-full rounded-2xl p-3 flex items-center gap-x-4 transition-all ease-in-out duration-300 group hover:bg-blue-light/25 lg:p-6 lg:gap-x-6">
         <figure class="bg-green-light/25 rounded-full w-14 h-14 flex items-center justify-center lg:w-16 lg:h-16">
@@ -11,8 +16,3 @@
         </div>
     </a>
 </li>
-
-<script lang="ts">
-    export let description: string = 'Name der Einkaufsliste';
-    export let count: number = 0;
-</script>
--- src/web-service/frontend/src/lib/navigation/NavBar.svelte
+++ src/web-service/frontend/src/lib/navigation/NavBar.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+    import NavItem from "./NavItem.svelte";
+    import Euro from "../../assets/svg/Euro.svelte";
+</script>
+
 <aside class="fixed bottom-0 bg-white w-full rounded-t-[2.5rem] px-5 py-4 drop-shadow-navBar sm:left-0 sm:top-0 sm:w-auto sm:rounded-none sm:rounded-r-2xl sm:px-4">
     <div class="hidden pt-10 pb-8 sm:block border-b-2 border-b-gray-dark/20">
         <figure class="w-12 h-12 bg-blue-dark/90 rounded-full flex items-center justify-center">
@@ -11,8 +16,3 @@
         <NavItem icon="profile"/>
     </nav>
 </aside>
-
-<script lang="ts">
-    import NavItem from "./NavItem.svelte";
-    import Euro from "../../assets/svg/Euro.svelte";
-</script>
