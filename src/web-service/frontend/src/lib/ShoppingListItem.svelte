f9a868ae4e57b67a8590d25ea67de7dac0cbca37
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2023-11-03 17:13:13 +0100

    refactor(web-services): update slugs for shopping lists

--- src/web-service/frontend/src/lib/ShoppingListItem.svelte
+++ src/web-service/frontend/src/lib/ShoppingListItem.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
     export let description: string = 'Name der Einkaufsliste';
     export let count: number = 0;
+    export let id: number = 0;
 </script>
 
 <li>
-    <a href="/" class="bg-white w-full rounded-2xl p-3 flex items-center gap-x-4 transition-all ease-in-out duration-300 group hover:bg-blue-light/25 lg:p-6 lg:gap-x-6">
+    <a href="/shopping-lists/{id}" class="bg-white w-full rounded-2xl p-3 flex items-center gap-x-4 transition-all ease-in-out duration-300 group hover:bg-blue-light/25 lg:p-6 lg:gap-x-6">
         <figure class="bg-green-light/25 rounded-full w-14 h-14 flex items-center justify-center lg:w-16 lg:h-16">
             <span class="text-2xl lg:text-3xl">🥗</span>
         </figure>
