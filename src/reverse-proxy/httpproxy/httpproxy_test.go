1758e92a2dac0457bdda093b5a6427c8e5acdc2b
Fabian Petersen <fabian@nf-petersen.de>
2023-11-04 10:24:32 +0100

    test(reverse-proxy): Add the rest of the tests to test the whole thing

--- src/reverse-proxy/httpproxy/httpproxy_test.go
+++ src/reverse-proxy/httpproxy/httpproxy_test.go
@@ -16,6 +16,30 @@ func TestHTTPProxy(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	client := mocks.NewMockClient(ctrl)
 
+	t.Run("throw an error if no hosts are provided", func(t *testing.T) {
+		// given
+		proxy := New(client)
+
+		// when
+		err := proxy.Add("*", "/the/route", []string{})
+
+		// then
+		assert.NotNil(t, err)
+		assert.Equal(t, "there was no host provided", err.Error())
+	})
+
+	t.Run("throw an error if hosts is not a valid url", func(t *testing.T) {
+		// given
+		proxy := New(client)
+
+		// when
+		err := proxy.Add("*", "/the/route", []string{"{not-valid-url}"})
+
+		// then
+		assert.NotNil(t, err)
+		assert.Equal(t, "invalid origin server URL", err.Error())
+	})
+
 	t.Run("should return 404 NOT FOUND if host is unknown", func(t *testing.T) {
 		// given
 		proxy := New(client)
@@ -87,6 +111,8 @@ func TestHTTPProxy(t *testing.T) {
 		assert.Equal(t, http.StatusOK, w.Code)
 		assert.Equal(t, "new-host:3000", r.Host)
 		assert.Equal(t, "http", r.URL.Scheme)
+		assert.Equal(t, r.RemoteAddr, r.Header.Get("X-Forwarded-For"))
+		assert.Equal(t, "example.com", r.Header.Get("X-Forwarded-Host"))
 	})
 
 	t.Run("should copy all header values from the server", func(t *testing.T) {
@@ -152,6 +178,47 @@ func TestHTTPProxy(t *testing.T) {
 		assert.Equal(t, "new-host:3000", r.Host)
 		assert.Equal(t, "http", r.URL.Scheme)
 		assert.Equal(t, requestBodyContent, responseBodyContent)
+	})
+
+	t.Run("should call both hosts after another", func(t *testing.T) {
+		// given
+		proxy := New(client)
+		proxy.Add("*", "/the/route", []string{"http://new-host:3000", "http://second-host:8000"})
+
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest("GET", "/the/route", nil)
+
+		response := &http.Response{
+			Status:        "OK",
+			StatusCode:    http.StatusOK,
+			Header:        http.Header{},
+			Body:          http.NoBody,
+			ContentLength: 0,
+			Request:       r,
+		}
+
+		// when
+		client.EXPECT().Do(r).Return(response, nil).Times(2)
+		proxy.ServeHTTP(w, r)
+
+		// then
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.Equal(t, "new-host:3000", r.Host)
+		assert.Equal(t, "http", r.URL.Scheme)
+
+		// given a second time
+		w = httptest.NewRecorder()
+		r.Host = "example.com"
+		r.Header.Del("X-Forwarded-For")
+		r.Header.Del("X-Forwarded-Host")
+
+		// when a second time
+		proxy.ServeHTTP(w, r)
+
+		// then
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.Equal(t, "second-host:8000", r.Host)
+		assert.Equal(t, "http", r.URL.Scheme)
 	})
 }
 
