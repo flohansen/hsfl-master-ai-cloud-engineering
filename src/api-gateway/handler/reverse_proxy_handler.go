fa7ae73b8d0b7b015ceafeba471db804d7972211
Finn Wessel <finn.wessel@stud.hs-flensburg.de>
2023-11-10 12:55:21 +0100

    feat(api-gateway): use regex to match targets

--- src/api-gateway/handler/reverse_proxy_handler.go
+++ src/api-gateway/handler/reverse_proxy_handler.go
@@ -1,31 +1,45 @@
 package handler
 
-import(
+import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"strings"
+	"regexp"
 )
+
+type Service struct {
+	Name         string
+	ContextPath  string
+	TargetURL    string
+	ReverseProxy *httputil.ReverseProxy
+}
 
 // ReverseProxyConfig represents the configuration for the Reverse Proxy.
 type ReverseProxyConfig struct {
-	AuthServiceURL       string
-	BulletinBoardServiceURL string
-	FeedServiceURL         string
+	Services []Service
 }
 
 // ReverseProxyHandler is a struct representing the Reverse Proxy.
 type ReverseProxyHandler struct {
-	AuthService       *httputil.ReverseProxy
-	BulletinBoardService *httputil.ReverseProxy
-	FeedService         *httputil.ReverseProxy
+	Services map[*regexp.Regexp]*httputil.ReverseProxy
 }
 
 func NewReverseProxy(config ReverseProxyConfig) *ReverseProxyHandler {
+	services := make(map[*regexp.Regexp]*httputil.ReverseProxy)
+
+	for _, service := range config.Services {
+		proxy := newSingleHostReverseProxy(service.TargetURL)
+		pattern, err := regexp.Compile(service.ContextPath)
+
+		if err != nil {
+			panic(err)
+		}
+
+		services[pattern] = proxy
+	}
+
 	return &ReverseProxyHandler{
-		AuthService:       newSingleHostReverseProxy(config.AuthServiceURL),
-		BulletinBoardService: newSingleHostReverseProxy(config.BulletinBoardServiceURL),
-		FeedService:          newSingleHostReverseProxy(config.FeedServiceURL),
+		Services: services,
 	}
 }
 func newSingleHostReverseProxy(targetURL string) *httputil.ReverseProxy {
@@ -33,60 +47,13 @@ func newSingleHostReverseProxy(targetURL string) *httputil.ReverseProxy {
 	return httputil.NewSingleHostReverseProxy(target)
 }
 
-func CleanPath(path string) string {
-	
-	// Split the path by "/"
-    segments := strings.Split(path, "/")
-
-    // Create a slice to store the non-empty segments
-    cleanSegments := make([]string, 0, len(segments))
-
-    // Iterate through the segments and add non-empty segments to the cleanSegments slice
-    for _, segment := range segments {
-        if segment != "" {
-            cleanSegments = append(cleanSegments, segment)
-        }
-    }
-
-    // If all segments were empty or the path was empty, return an empty string
-    if len(cleanSegments) == 0 {
-        return ""
-    }
-
-    // Join the clean segments with "/" to create the cleaned path
-    cleanPath := strings.Join(cleanSegments, "/")
-
-    return cleanPath
-}
-func (rp *ReverseProxyHandler) determineTarget(path string) string {
-    if(path == ""){
-		return "unknown"
-	}
-	cleanPath := CleanPath(path)
-	segments := strings.Split(cleanPath, "/")
-
-    // Determine the target based on the first segment
-    if len(segments) > 0 {
-        return segments[0]
-    }
-
-    // Default to a target of "unknown" if the path is empty
-    return "unknown"
-}
-
 func (rp *ReverseProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// Determine the target service based on the request path
-	target := rp.determineTarget(r.URL.Path)
-
-	// Route the request to the appropriate service
-	switch target {
-	case "auth":
-		rp.AuthService.ServeHTTP(w, r)
-	case "bulletinboard":
-		rp.BulletinBoardService.ServeHTTP(w, r)
-	case "feed":
-		rp.FeedService.ServeHTTP(w, r)
-	default:
-		http.Error(w, "Service not found", http.StatusNotFound)
+	for pattern, proxy := range rp.Services {
+		if pattern.MatchString(r.URL.Path) {
+			proxy.ServeHTTP(w, r)
+			return
+		}
 	}
+
+	http.Error(w, "Service not found", http.StatusNotFound)
 }
--- src/api-gateway/main.go
+++ src/api-gateway/main.go
@@ -2,21 +2,17 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"github.com/Flo0807/hsfl-master-ai-cloud-engineering/src/api-gateway/handler"
-
+	"net/http"
 )
-
 
 func main() {
 	// Configuration for the Reverse Proxy
 	config := handler.ReverseProxyConfig{
-
-		//TODO CORRECT PORTS AND PATHS
-
-		//AuthServiceURL:       "http://authservice:port",
-		//BulletinBoardServiceURL: "http://bulletinboardservice:port",
-		FeedServiceURL:         "http://localhost:8081",
+		Services: []handler.Service{
+			{Name: "auth", ContextPath: "/auth", TargetURL: "http://localhost:8081"},
+			{Name: "bulletinboard", ContextPath: "/bulletin-board", TargetURL: "http://localhost:8080"},
+		},
 	}
 
 	// Create a new Reverse Proxy
@@ -26,9 +22,9 @@ func main() {
 	http.Handle("/", reverseProxy)
 
 	// Start the Reverse Proxy on a port
-	port := 8080
+	port := 8000
 	fmt.Printf("Reverse Proxy is running on port %d\n", port)
-	
+
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
