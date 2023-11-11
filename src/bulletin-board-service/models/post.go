ea9060cc20bdebbd4e51ceef5cde15295c2bf51c
Finn Wessel <finn.wessel@stud.hs-flensburg.de>
2023-11-11 22:27:40 +0100

    feat(bulletin-board-service): Update json representation

--- src/bulletin-board-service/models/post.go
+++ src/bulletin-board-service/models/post.go
@@ -6,9 +6,9 @@ import (
 
 // Post struct represents a bulletin board post
 type Post struct {
-	ID        uint `gorm:"primary_key" json:"id"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        uint       `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time  `json:"createdAt"`
+	UpdatedAt time.Time  `json:"updatedAt"`
 	DeletedAt *time.Time `sql:"index"`
 
 	Title   string `json:"title"`
