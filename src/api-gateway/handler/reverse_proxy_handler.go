a16a16d8f9830a928e23bb8fe6f1718cd4379364
Finn Wessel <finn.wessel@stud.hs-flensburg.de>
2023-11-11 16:16:26 +0100

    feat(api-gateway): add service sorting by specificity of context path

--- src/api-gateway/handler/reverse_proxy_handler.go
+++ src/api-gateway/handler/reverse_proxy_handler.go
@@ -6,13 +6,19 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"regexp"
+	"sort"
 )
 
 type Service struct {
 	Name         string
 	ContextPath  string
 	TargetURL    string
+	Pattern      *regexp.Regexp
 	ReverseProxy *httputil.ReverseProxy
+}
+
+type ReverseProxyHandler struct {
+	Services []Service
 }
 
 // ReverseProxyConfig represents the configuration for the Reverse Proxy.
@@ -21,28 +27,7 @@ type ReverseProxyConfig struct {
 }
 
 // ReverseProxyHandler is a struct representing the Reverse Proxy.
-type ReverseProxyHandler struct {
-	Services map[*regexp.Regexp]*httputil.ReverseProxy
-}
 
-func NewReverseProxy(config ReverseProxyConfig) *ReverseProxyHandler {
-	services := make(map[*regexp.Regexp]*httputil.ReverseProxy)
-
-	for _, service := range config.Services {
-		proxy := newSingleHostReverseProxy(service.TargetURL)
-		pattern, err := regexp.Compile(service.ContextPath)
-
-		if err != nil {
-			panic(err)
-		}
-
-		services[pattern] = proxy
-	}
-
-	return &ReverseProxyHandler{
-		Services: services,
-	}
-}
 func newSingleHostReverseProxy(targetURL string) *httputil.ReverseProxy {
 	target, _ := url.Parse(targetURL)
 	proxy := httputil.NewSingleHostReverseProxy(target)
@@ -53,11 +38,37 @@ func newSingleHostReverseProxy(targetURL string) *httputil.ReverseProxy {
 	return proxy
 }
 
+func NewReverseProxy(config ReverseProxyConfig) *ReverseProxyHandler {
+	var services []Service
+
+	sort.Slice(config.Services, func(i, j int) bool {
+		return len(config.Services[i].ContextPath) > len(config.Services[j].ContextPath)
+	})
+
+	for _, service := range config.Services {
+		proxy := newSingleHostReverseProxy(service.TargetURL)
+		pattern, err := regexp.Compile("^" + service.ContextPath)
+
+		if err != nil {
+			panic(err)
+		}
+
+		service.Pattern = pattern
+		service.ReverseProxy = proxy
+
+		services = append(services, service)
+	}
+
+	return &ReverseProxyHandler{
+		Services: services,
+	}
+}
+
 func (rp *ReverseProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	for pattern, proxy := range rp.Services {
-		if pattern.MatchString(r.URL.Path) {
-			log.Printf("Proxying %s to %s", r.URL.Path, pattern.String())
-			proxy.ServeHTTP(w, r)
+	for _, service := range rp.Services {
+		if service.Pattern.MatchString(r.URL.Path) {
+			log.Printf("Proxying %s to %s", r.URL.Path, service.Pattern.String())
+			service.ReverseProxy.ServeHTTP(w, r)
 			return
 		}
 	}
