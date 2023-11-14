8a2a3a6097c5e4afc9be67e015bcda2fe03a920b
Philipp Borucki <p.borucki@icloud.com>
2023-11-14 17:32:21 +0100

    feat(load-balancer-service): add Endpoint struct skeleton

---
+++ src/load-balancer-service/balancer/endpoint/endpoint.go
@@ -0,0 +1,31 @@
+package endpoint
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+type Endpoint struct {
+	proxy           *httputil.ReverseProxy
+	currentRequests int
+}
+
+func NewEndpoint(url *url.URL) *Endpoint {
+	return &Endpoint{
+		proxy:           httputil.NewSingleHostReverseProxy(url),
+		currentRequests: 0,
+	}
+}
+
+func (e *Endpoint) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	e.currentRequests += 1
+	defer func() {
+		e.currentRequests -= 1
+	}()
+	e.proxy.ServeHTTP(w, r)
+}
+
+func (e *Endpoint) IsAvailable() bool {
+	return true
+}
