d72e8e1d7b0aa605ac33141feb8629cea4383548
Philipp Borucki <p.borucki@icloud.com>
2023-11-17 18:06:34 +0100

    feat(load-balancer-service): add health check setter to balancer

--- src/load-balancer-service/balancer/balancer.go
+++ src/load-balancer-service/balancer/balancer.go
@@ -2,9 +2,12 @@ package balancer
 
 import (
 	"hsfl.de/group6/hsfl-master-ai-cloud-engineering/load-balancer-service/balancer/endpoint"
+	"hsfl.de/group6/hsfl-master-ai-cloud-engineering/load-balancer-service/balancer/endpoint/health"
 	"hsfl.de/group6/hsfl-master-ai-cloud-engineering/load-balancer-service/balancer/scheduler"
+	"log"
 	"net/http"
 	"net/url"
+	"time"
 )
 
 type Balancer struct {
@@ -27,8 +30,15 @@ func NewBalancer(targetUrls []*url.URL, scheduler scheduler.NewScheduler) *Balan
 func (b *Balancer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	endpoint, err := (*b.schedulerAlgorithm).Next()
 	if err != nil {
+		log.Printf(err.Error())
 		w.WriteHeader(http.StatusServiceUnavailable)
 		return
 	}
 	endpoint.ServeHTTP(w, r)
+}
+
+func (b *Balancer) SetHealthCheckFunction(check health.CheckFunction, period time.Duration) {
+	for _, endpoint := range b.endpoints {
+		endpoint.SetHealthCheckFunction(check, period)
+	}
 }
