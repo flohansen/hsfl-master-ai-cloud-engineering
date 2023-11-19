9261c2f5b1cf7cd64cdc028c520ca9a4fd387344
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2023-11-19 14:35:05 +0100

    fix(user-service): update router tests

--- src/user-service/api/router/router_test.go
+++ src/user-service/api/router/router_test.go
@@ -21,6 +21,18 @@ func TestRouter(t *testing.T) {
 	pricesController := user.NewDefaultController(userRepo)
 	router := New(loginHandler, registerHandler, pricesController)
 
+	t.Run("should return 404 NOT FOUND if path is unknown", func(t *testing.T) {
+		// given
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest("GET", "/unknown/route", nil)
+
+		// when
+		router.ServeHTTP(w, r)
+
+		// then
+		assert.Equal(t, http.StatusNotFound, w.Code)
+	})
+
 	t.Run("/api/v1/user/login", func(t *testing.T) {
 		t.Run("should return 404 NOT FOUND if method is not POST", func(t *testing.T) {
 			tests := []string{"HEAD", "CONNECT", "OPTIONS", "TRACE", "PATCH", "GET", "DELETE", "PUT"}
@@ -34,7 +46,11 @@ func TestRouter(t *testing.T) {
 				router.ServeHTTP(w, r)
 
 				// then
-				assert.Equal(t, http.StatusNotFound, w.Code)
+				if test == "GET" || test == "PUT" || test == "DELETE" {
+					assert.Equal(t, http.StatusBadRequest, w.Code)
+				} else {
+					assert.Equal(t, http.StatusNotFound, w.Code)
+				}
 			}
 		})
 
@@ -51,6 +67,7 @@ func TestRouter(t *testing.T) {
 			assert.Equal(t, http.StatusOK, w.Code)
 		})
 	})
+
 	t.Run("/api/v1/user/register", func(t *testing.T) {
 		t.Run("should return 404 NOT FOUND if method is not POST", func(t *testing.T) {
 			tests := []string{"HEAD", "CONNECT", "OPTIONS", "TRACE", "PATCH", "GET", "DELETE", "PUT"}
@@ -64,7 +81,11 @@ func TestRouter(t *testing.T) {
 				router.ServeHTTP(w, r)
 
 				// then
-				assert.Equal(t, http.StatusNotFound, w.Code)
+				if test == "GET" || test == "PUT" || test == "DELETE" {
+					assert.Equal(t, http.StatusBadRequest, w.Code)
+				} else {
+					assert.Equal(t, http.StatusNotFound, w.Code)
+				}
 			}
 		})
 
@@ -72,6 +93,74 @@ func TestRouter(t *testing.T) {
 			// given
 			w := httptest.NewRecorder()
 			r := httptest.NewRequest("POST", "/api/v1/user/register", strings.NewReader(`{"email": "grace.hopper@gmail.com", "password": "123456", "name": "Grace Hopper", "role": 0}`))
+
+			// when
+			router.ServeHTTP(w, r)
+
+			// then
+			assert.Equal(t, http.StatusOK, w.Code)
+		})
+	})
+
+	t.Run("/api/v1/user/:userId", func(t *testing.T) {
+		t.Run("should return 404 NOT FOUND if method is not GET, DELETE, PUT or POST", func(t *testing.T) {
+			tests := []string{"HEAD", "CONNECT", "OPTIONS", "TRACE", "PATCH"}
+
+			for _, test := range tests {
+				// given
+				w := httptest.NewRecorder()
+				r := httptest.NewRequest(test, "/api/v1/user/1", nil)
+
+				// when
+				router.ServeHTTP(w, r)
+
+				// then
+				assert.Equal(t, http.StatusNotFound, w.Code)
+			}
+		})
+
+		t.Run("should call GET handler", func(t *testing.T) {
+			// given
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest("GET", "/api/v1/user/1", nil)
+
+			// when
+			router.ServeHTTP(w, r)
+
+			// then
+			assert.Equal(t, http.StatusOK, w.Code)
+		})
+
+		t.Run("should call PUT handler", func(t *testing.T) {
+			// given
+			w := httptest.NewRecorder()
+			jsonRequest := `{"id": 1, "email": "updated@googlemail.com", "name": "Updated user"}`
+			r := httptest.NewRequest("PUT", "/api/v1/user/1", strings.NewReader(jsonRequest))
+
+			// when
+			router.ServeHTTP(w, r)
+
+			// then
+			assert.Equal(t, http.StatusOK, w.Code)
+		})
+
+		t.Run("should call POST handler", func(t *testing.T) {
+			// given
+			w := httptest.NewRecorder()
+			jsonRequest := `{"id": 3, "email": "example@googlemail.com", "password": "password", "name": "Example name"}`
+			r := httptest.NewRequest("POST", "/api/v1/user", strings.NewReader(jsonRequest))
+
+			// when
+			router.ServeHTTP(w, r)
+
+			// then
+			assert.Equal(t, http.StatusCreated, w.Code)
+		})
+
+		t.Run("should call DELETE handler", func(t *testing.T) {
+			// given
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest("DELETE", "/api/v1/user/1", nil)
 
 			// when
 			router.ServeHTTP(w, r)
