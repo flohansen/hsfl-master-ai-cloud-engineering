a4b4f68ba9a13f63851d18f55914eb9ac154f6b1
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2023-11-19 10:52:00 +0100

    fix(web-service): view change

--- src/web-service/frontend/src/lib/shopplig-list/ViewSelect.svelte
+++ src/web-service/frontend/src/lib/shopplig-list/ViewSelect.svelte
@@ -9,7 +9,7 @@
 <div class="flex items-center mt-10 justify-end">
     <button
         aria-label="Einkaufstliste mit Details anzeigen"
-        on:click={() => view  === 'detailed'}
+        on:click={() => view = 'detailed'}
         class="rounded-l-xl py-1 pl-2.5 pr-1 transition-all ease-in-out duration-300 cursor-pointer hover:bg-gray-dark/25 lg:py-2 lg:pl-4 lg:pr-2
             {view === 'detailed' ? 'bg-green-light/50 text-green-dark' : 'bg-gray-light text-black'}">
         <Tiles classes="w-5 h-5"/>
@@ -17,7 +17,7 @@
 
     <button
         aria-label="Einkaufstliste ohne Details anzeigen"
-        on:click={() => view  === 'compressed'}
+        on:click={() => view = 'compressed'}
         class="rounded-r-xl py-1 pr-2.5 pl-1 transition-all ease-in-out duration-300 cursor-pointer hover:bg-gray-dark/25 lg:py-2 lg:pr-4 lg:pl-2
             {view === 'compressed' ? 'bg-green-light/50 text-green-dark' : 'bg-gray-light text-black'}">
         <List classes="w-5 h-5"/>
