84916d3aff7a14bf1635fc1dde095d863f4fe8af
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2023-11-19 12:05:15 +0100

    feat(web-service): add possibility to delete shopping lists

--- src/web-service/frontend/src/lib/shopplig-list/ShoppingList.svelte
+++ src/web-service/frontend/src/lib/shopplig-list/ShoppingList.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
+    import Trash from "../../assets/svg/Trash.svelte";
+    import {handleErrors} from "../../assets/helper/handleErrors";
+
     export let description: string = 'Name der Einkaufsliste';
     export let id: number = 0;
+
+    function deleteList() : void {
+        const apiUrl: string = `/api/v1/shoppinglist/${id}`
+        const requestOptions = {
+            method: "DELETE",
+            headers: { 'Content-Type': 'application/json' },
+        };
+
+        fetch(apiUrl, requestOptions)
+            .then(handleErrors)
+            .then(()=> location.reload())
+            .catch(error => console.error("Failed to fetch data:", error.message));
+    }
 </script>
 
-<li>
-    <a href="/shopping-lists/{id}" class="bg-white w-full rounded-2xl p-3 flex items-center gap-x-4 transition-all ease-in-out duration-300 group hover:bg-blue-light/25 lg:p-6 lg:gap-x-6">
+<li class="bg-white w-full rounded-2xl flex items-center justify-between transition-all ease-in-out duration-300 group hover:bg-blue-light/25">
+    <a href="/shopping-lists/{id}" class="w-full p-3 lg:p-6 flex items-center gap-x-4 lg:gap-x-6">
         <figure class="bg-green-light/25 rounded-full w-14 h-14 flex items-center justify-center lg:w-16 lg:h-16">
             <span class="text-2xl lg:text-3xl">🥗</span>
         </figure>
@@ -17,4 +33,10 @@
             </p>
         </div>
     </a>
+    <button
+        aria-label="Einkaufsliste löschen"
+        on:click={deleteList}
+        class="h-full p-3 lg:p-6 border-l border-l-blue-light">
+        <Trash classes="w-5 h-5 text-blue-dark/50" />
+    </button>
 </li>
