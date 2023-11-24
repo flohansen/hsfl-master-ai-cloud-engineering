42ce6ee135ccfa0473ad84070a4c77b4d7a48119
Philipp Borucki <p.borucki@icloud.com>
2023-11-24 12:34:44 +0100

    chore(load-balancer-service): clean up main

--- src/load-balancer-service/main.go
+++ src/load-balancer-service/main.go
@@ -14,31 +14,39 @@ import (
 )
 
 func main() {
-	defaultOrchestrator := orchestrator.NewDefaultOrchestrator()
+	// Parse command line arguments
 	image := flag.String("image", "nginxdemos/hello", "")
 	replicas := flag.Int("replicas", 5, "")
 	networkName := flag.String("network", "bridge", "")
 	flag.Parse()
+
+	// Log configuration
 	log.Printf("Image is: %s", *image)
 	log.Printf("No. of Replicas is: %d", *replicas)
 	log.Printf("Network is: %s", *networkName)
 
+	// Initialize orchestrator and start containers
+	var defaultOrchestrator orchestrator.Orchestrator = orchestrator.NewDefaultOrchestrator()
 	containers := defaultOrchestrator.StartContainers(*image, *replicas, *networkName)
 
+	// Initialize load balancer
 	loadBalancer := balancer.NewBalancer(
 		defaultOrchestrator.GetContainerEndpoints(containers, *networkName),
 		scheduler.NewRoundRobin)
 	loadBalancer.SetHealthCheckFunction(health.DefaultHealthCheck, 5*time.Second)
 
+	// Start web server
 	server := &http.Server{
 		Addr:    ":3000",
 		Handler: loadBalancer,
 	}
 
+	//
 	go func() {
 		log.Print(server.ListenAndServe())
 	}()
 
+	// Graceful shutdown
 	wait := utils.GracefulShutdown(context.Background(), 30*time.Second, map[string]utils.Operation{
 		"orchestrator": func(ctx context.Context) error {
 			return defaultOrchestrator.Shutdown(ctx)
