69c8380f5f84d971fd24b08cdf3d87bcba078cb4
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2023-12-01 12:47:41 +0100

    feat(web-service): show all merchants

--- src/web-service/frontend/src/routes/merchants/+page.svelte
+++ src/web-service/frontend/src/routes/merchants/+page.svelte
@@ -1,10 +1,46 @@
 <script lang="ts">
     import {page} from "$app/stores";
     import Header from "$lib/general/Header.svelte";
+
+    interface Merchant {
+        id: number;
+        name: string;
+        email: string;
+        role?: number;
+        productsCount?: number;
+    }
+
+    interface Data {
+        merchants: Merchant[],
+        headline: string;
+    }
+
+    export let data: Data;
 </script>
 
 <Header headline="{$page.data.headline}"/>
 
 <main>
-    <p class="mx-5">TODO: Add content about merchants</p>
+    <ul class="px-5 mt-4 grid grid-cols-1 gap-y-4 lg:gap-y-6 lg:mt-6">
+        {#if data.merchants}
+            {#each data.merchants as merchant}
+                <li class="bg-white w-full rounded-2xl flex items-center justify-between transition-all ease-in-out duration-300 group hover:bg-blue-light/25">
+                    <a href="/" class="w-full px-3 py-5 lg:px-6 lg:py-8">
+                        <div class="text-left">
+                            <h3 class="font-semibold text-base transition-all ease-in-out duration-300 group-hover:text-blue-dark lg:text-lg">
+                                {merchant.name}
+                            </h3>
+                            {#if merchant.productsCount}
+                                <p class="text-xs text-gray-dark mt-1 lg:text-sm">
+                                    {merchant.productsCount} Produkte
+                                </p>
+                            {/if}
+                        </div>
+                    </a>
+                </li>
+            {/each}
+        {:else}
+            <p>Es konnten keine Daten geladen werden.</p>
+        {/if}
+    </ul>
 </main>
--- src/web-service/frontend/src/routes/merchants/+page.ts
+++ src/web-service/frontend/src/routes/merchants/+page.ts
@@ -1,6 +1,55 @@
-export function load(): object {
+import {handleErrors} from "../../assets/helper/handleErrors";
+
+interface User {
+    id: number;
+    name: string;
+    email: string;
+    role?: number;
+    productsCount?: number;
+}
+
+interface Price {
+    userId: number;
+    productId: number;
+    price: number;
+}
+
+export const load = async (): Promise<object> => {
+    const apiUrlUsers: string = '/api/v1/user';
+    const apiUrlPrices: string = '/api/v1/price';
+
+    const [users, prices] = await Promise.all([
+        fetch(apiUrlUsers).then(handleErrors),
+        fetch(apiUrlPrices).then(handleErrors),
+    ]);
+
+    if (users === null || prices === null) {
+        console.warn('Products or users not found');
+    }
+
     return {
+        merchants: users ? getMerchantsContent(users, prices) : [],
         metaTitle: 'Auflistung der Supermärkte',
         headline: 'Alle verfügbaren Supermärkte',
     };
+};
+
+function getMerchantsContent(users: User[], prices: Price[]): User[] {
+    const merchants: User[] = filterUserByRole(users);
+
+    merchants.forEach(merchant => {
+        merchant.productsCount = calculateProductsCount(merchant.id, prices);
+    });
+
+    return merchants;
+
+}
+
+function calculateProductsCount(merchantId: number, prices: Price[]): number {
+    const merchantProducts: Price[] = prices.filter(price => price.userId === merchantId);
+    return merchantProducts.length;
+}
+
+function filterUserByRole(users: User[]): User[] {
+    return users.filter(user => user.role === 1);
 }
