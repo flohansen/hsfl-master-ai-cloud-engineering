9eb375749c8ed1a0ed690f2aa106e3a286660b1c
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2023-12-03 12:48:43 +0100

    feat(web-service): update merchant fetch data

--- src/web-service/frontend/src/routes/merchants/+page.ts
+++ src/web-service/frontend/src/routes/merchants/+page.ts
@@ -1,9 +1,8 @@
 import {handleErrors} from "../../assets/helper/handleErrors";
 
-interface User {
+interface Merchant {
     id: number;
     name: string;
-    email: string;
     role?: number;
     productsCount?: number;
 }
@@ -15,41 +14,34 @@ interface Price {
 }
 
 export const load = async (): Promise<object> => {
-    const apiUrlUsers: string = '/api/v1/user';
+    const apiUrlUsers: string = '/api/v1/user/role/1';
     const apiUrlPrices: string = '/api/v1/price';
 
-    const [users, prices] = await Promise.all([
+    const [merchants, prices] = await Promise.all([
         fetch(apiUrlUsers).then(handleErrors),
         fetch(apiUrlPrices).then(handleErrors),
     ]);
 
-    if (users === null || prices === null) {
+    if (merchants === null || prices === null) {
         console.warn('Products or users not found');
     }
 
     return {
-        merchants: users ? getMerchantsContent(users, prices) : [],
+        merchants: merchants ? getMerchantsContent(merchants, prices) : [],
         metaTitle: 'Auflistung der Supermärkte',
         headline: 'Alle verfügbaren Supermärkte',
     };
 };
 
-function getMerchantsContent(users: User[], prices: Price[]): User[] {
-    const merchants: User[] = filterUserByRole(users);
-
+function getMerchantsContent(merchants: Merchant[], prices: Price[]): Merchant[] {
     merchants.forEach(merchant => {
         merchant.productsCount = calculateProductsCount(merchant.id, prices);
     });
 
     return merchants;
-
 }
 
 function calculateProductsCount(merchantId: number, prices: Price[]): number {
     const merchantProducts: Price[] = prices.filter(price => price.userId === merchantId);
     return merchantProducts.length;
-}
-
-function filterUserByRole(users: User[]): User[] {
-    return users.filter(user => user.role === 1);
 }
