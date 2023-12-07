cb6be8f8c2cfeb100c88b60e6e99fa3b0c2ee7cb
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2023-12-07 16:58:46 +0100

    feat(web-service): add shopping lists to startpage

--- src/web-service/frontend/src/routes/+page.ts
+++ src/web-service/frontend/src/routes/+page.ts
@@ -1,6 +1,24 @@
-export function load(): object {
-    return {
-        metaTitle: 'Startseite',
-        headline: 'Price Whisper',
-    };
-}
+import {handleErrors} from "../assets/helper/handleErrors";
+
+// Loads all shopping lists of the current user.
+export const load = (): Promise<object> => {
+    const id: number = 2; // TODO: dynamic user id of the current logged-in user
+    const apiUrl: string = `/api/v1/shoppinglist/${id}`;
+
+    return fetch(apiUrl)
+        .then(handleErrors)
+        .then(lists => {
+            return {
+                lists: lists.slice(0, 3),
+                metaTitle: 'Startseite',
+                headline: 'Price Whisper',
+            };
+        })
+        .catch(error => {
+            console.error("Failed to fetch shopping lists data:", error.message);
+            return {
+                metaTitle: 'Error',
+                headline: 'Price Whisper',
+            };
+        });
+};
