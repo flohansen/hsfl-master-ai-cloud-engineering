44cc735fe0a7d708558560d9e9f800053f23b24b
Philipp Borucki <p.borucki@icloud.com>
2024-01-02 22:10:58 +0100

    test(user-service): add random key generation for tests

--- src/user-service/api/http/handler/login_test.go
+++ src/user-service/api/http/handler/login_test.go
@@ -1,7 +1,12 @@
 package handler
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
 	"github.com/stretchr/testify/assert"
 	"hsfl.de/group6/hsfl-master-ai-cloud-engineering/user-service/auth"
 	"hsfl.de/group6/hsfl-master-ai-cloud-engineering/user-service/crypto"
@@ -184,8 +189,25 @@ func TestLoginHandler(t *testing.T) {
 }
 
 func setupLoginHandler() *LoginHandler {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		panic("Error generating private key for testing.")
+	}
+
+	derFormatKey, err := x509.MarshalECPrivateKey(privateKey)
+	if err != nil {
+		panic("Error converting ECDSA key to DER format")
+	}
+
+	pemKey := pem.EncodeToMemory(&pem.Block{
+		Type:  "EC PRIVATE KEY",
+		Bytes: derFormatKey,
+	})
+
+	pemPrivateKey := string(pemKey)
+
 	var jwtToken, _ = auth.NewJwtTokenGenerator(
-		auth.JwtConfig{PrivateKey: "../../auth/test-token"})
+		auth.JwtConfig{PrivateKey: pemPrivateKey})
 
 	return NewLoginHandler(setupMockRepository(),
 		crypto.NewBcryptHasher(), jwtToken)
