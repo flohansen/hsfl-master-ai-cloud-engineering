984fe25e691d31f954946f8ffe8804c07047b0bb
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2024-01-07 13:40:16 +0100

    refactor(benchmark)!: improve loadtest

--- src/benchmark/loadtest/loadtest.go
+++ src/benchmark/loadtest/loadtest.go
@@ -1,9 +1,12 @@
 package loadtest
 
 import (
+	"fmt"
 	"log"
 	"math/rand"
+	"net"
 	"net/http"
+	"net/url"
 	"sync"
 	"time"
 
@@ -29,21 +32,26 @@ func NewLoadTest(config *config.LoadTestConfig) *LoadTest {
 	}
 }
 
+func CalculateDuration(specs []config.Spec) time.Duration {
+	var duration time.Duration
+	for _, spec := range specs {
+		duration += spec.Duration
+	}
+	return duration
+}
+
 func (l *LoadTest) Run() {
-	rampupDuration := time.Duration(l.config.RampUp) * time.Second
-	duration := time.Duration(l.config.Duration) * time.Second
-	overallDuration := rampupDuration + duration
-	targets := l.config.Targets
+	log.Println("Starting load test")
+
 	users := l.config.Users
+	specs := l.config.Specs
+	targets := l.config.Targets
+	startSleep := l.config.StartSleep
 
-	log.Println("Starting load test with ", users, " users for ", overallDuration, " seconds")
-
-	httpClient := &http.Client{}
+	targetSleep := specs[0].Sleep
+	overallDuration := CalculateOverallDuration(specs)
 
 	var wg sync.WaitGroup
-
-	responseTimesByTarget := make(map[string]responseTimeEntry)
-	var mu sync.Mutex
 
 	for i := 0; i < users; i++ {
 		wg.Add(1)
@@ -54,42 +62,120 @@ func (l *LoadTest) Run() {
 			stop := time.NewTimer(overallDuration)
 			start := time.Now()
 
+			lastStage := 0
+			lastSleepDuration := startSleep
+			targetSleepDuration := targetSleep
+
 			for {
 				select {
 				case <-stop.C:
 					return
 				default:
-					target := PickRandomTarget(targets)
+					log.Print("Time since start: ", time.Since(start))
 
-					go func() {
-						err := MakeRequest(httpClient, target, id, responseTimesByTarget, &mu)
-						if err != nil {
-							log.Println("Error making request: ", err)
-						}
-					}()
+					t := time.Now()
 
-					elapsed := time.Since(start)
-					sleepDuration := CalculateSleepDuration(elapsed, rampupDuration)
-					time.Sleep(sleepDuration)
+					currentStage := GetCurrentStage(specs, time.Since(start))
+					elapsedStageTime := CalculateStageElapsedTime(specs, currentStage, time.Since(start))
+					currentStageDuration := specs[currentStage].Duration
+
+					if currentStage != lastStage {
+						lastSleepDuration = targetSleepDuration
+						targetSleepDuration = specs[currentStage].Sleep
+
+						lastStage = currentStage
+					}
+
+					sleepDuration := CalculateSleepDuration(elapsedStageTime, currentStageDuration, lastSleepDuration, targetSleepDuration)
+
+					url, err := PickRandomURL(targets)
+
+					if err != nil {
+						log.Println("Error picking random URL: ", err)
+						continue
+					}
+
+					code, err := DoRequest(url)
+
+					log.Println("User ", id, " made request with status code ", code, " and error ", err)
+
+					time.Sleep(sleepDuration - time.Since(t))
 				}
 			}
 		}(i)
 	}
 
 	wg.Wait()
-
-	log.Println("Finished load test")
-
-	PrintAverageResponseTimes(responseTimesByTarget)
 }
 
-func CalculateSleepDuration(elapsed time.Duration, rampupDuration time.Duration) time.Duration {
-	if elapsed < rampupDuration {
-		// linearly decrease sleep duration from maximum duration to minimumSleepDuration
-		remainingPortion := 1.0 - (float64(elapsed) / float64(rampupDuration))
-		return MinimumSleepDuration + time.Duration(remainingPortion*DecreaseInterval)
+func DoRequest(target *url.URL) (uint64, error) {
+	conn, err := net.Dial("tcp", target.Host)
+	if err != nil {
+		return 0, err
+	}
+
+	defer conn.Close()
+
+	_, err = fmt.Fprintf(conn, "GET %s HTTP/1.1\r\nHost: go\r\n\r\n", target.Path)
+	if err != nil {
+		return 0, err
+	}
+
+	code := uint64(200)
+
+	return code, nil
+}
+
+func GetCurrentStage(specs []config.Spec, elapsed time.Duration) int {
+	var stage int
+	var stageDuration time.Duration
+
+	for i, spec := range specs {
+		stageDuration += spec.Duration
+
+		if elapsed < stageDuration {
+			stage = i
+			break
+		}
+	}
+	return stage
+}
+
+func CalculateOverallDuration(specs []config.Spec) time.Duration {
+	var duration time.Duration
+
+	for _, spec := range specs {
+		duration += spec.Duration
+	}
+
+	return duration
+}
+
+func CalculateStageElapsedTime(specs []config.Spec, currentStage int, elapsed time.Duration) time.Duration {
+	var stageDuration time.Duration
+
+	if currentStage == 0 {
+		return elapsed
+	}
+
+	for i := 0; i < currentStage; i++ {
+		stageDuration += specs[i].Duration
+	}
+
+	return elapsed - stageDuration
+}
+
+func CalculateSleepDuration(elapsed time.Duration, duration time.Duration, lastSleep time.Duration, targetSleep time.Duration) time.Duration {
+	if lastSleep < targetSleep {
+		interval := float64(targetSleep - lastSleep)
+		remainingPortion := (float64(elapsed) / float64(duration))
+
+		return lastSleep + time.Duration(remainingPortion*interval)
 	} else {
-		return MinimumSleepDuration
+		interval := float64(lastSleep - targetSleep)
+		remainingPortion := 1.0 - (float64(elapsed) / float64(duration))
+
+		return targetSleep + time.Duration(remainingPortion*interval)
 	}
 }
 
@@ -100,8 +186,14 @@ func PrintAverageResponseTimes(responseTimesByTarget map[string]responseTimeEntr
 	}
 }
 
-func PickRandomTarget(targets []string) string {
-	return targets[rand.Intn(len(targets))]
+func PickRandomURL(targets []string) (*url.URL, error) {
+	url, err := url.Parse(targets[rand.Intn(len(targets))])
+
+	if err != nil {
+		return nil, err
+	}
+
+	return url, nil
 }
 
 func MakeRequest(httpClient *http.Client, target string, id int, responseTimesByTarget map[string]responseTimeEntry, mu *sync.Mutex) error {
