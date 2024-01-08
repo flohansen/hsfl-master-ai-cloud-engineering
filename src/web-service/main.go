07514f088d22548b24c627c33eefbb3ee43bb35b
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2024-01-08 17:30:42 +0100

    fix(web-service): import correct config

--- src/web-service/main.go
+++ src/web-service/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/Flo0807/hsfl-master-ai-cloud-engineering/load-balancer/config"
+	"github.com/Flo0807/hsfl-master-ai-cloud-engineering/web-service/config"
 	"github.com/caarlos0/env/v10"
 	"github.com/joho/godotenv"
 )
