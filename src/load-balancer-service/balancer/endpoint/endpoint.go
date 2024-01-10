e785005b242d9141cf58b85e064c2d6f48099a27
Jannick <jannick.lindner@hotmail.de>
2024-01-10 13:30:25 +0100

    test(load-balancer-service): add tests for scheduler

--- src/load-balancer-service/balancer/endpoint/endpoint.go
+++ src/load-balancer-service/balancer/endpoint/endpoint.go
@@ -9,52 +9,52 @@ import (
 )
 
 type Endpoint struct {
-	url              *url.URL
-	proxy            *httputil.ReverseProxy
-	currentRequests  int
-	lastResponseTime time.Duration
-	healthCheck      *health.HealthCheck
+	Url              *url.URL
+	Proxy            *httputil.ReverseProxy
+	CurrentRequests  int
+	LastResponseTime time.Duration
+	HealthCheck      *health.HealthCheck
 }
 
 func NewEndpoint(url *url.URL) *Endpoint {
 	return &Endpoint{
-		url:              url,
-		proxy:            httputil.NewSingleHostReverseProxy(url),
-		currentRequests:  0,
-		lastResponseTime: 0,
+		Url:              url,
+		Proxy:            httputil.NewSingleHostReverseProxy(url),
+		CurrentRequests:  0,
+		LastResponseTime: 0,
 	}
 }
 
 func (e *Endpoint) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	startTime := time.Now()
 
-	e.currentRequests += 1
+	e.CurrentRequests += 1
 	defer func() {
-		e.currentRequests -= 1
-		e.lastResponseTime = time.Since(startTime)
+		e.CurrentRequests -= 1
+		e.LastResponseTime = time.Since(startTime)
 	}()
-	e.proxy.ServeHTTP(w, r)
+	e.Proxy.ServeHTTP(w, r)
 }
 
 func (e *Endpoint) IsAvailable() bool {
-	if e.healthCheck != nil {
-		return e.healthCheck.IsAvailable()
+	if e.HealthCheck != nil {
+		return e.HealthCheck.IsAvailable()
 	}
 	return true
 }
 
 func (e *Endpoint) SetHealthCheckFunction(check health.CheckFunction, period time.Duration) {
-	e.healthCheck = health.NewHealthCheck(e.url, check, period)
+	e.HealthCheck = health.NewHealthCheck(e.Url, check, period)
 }
 
 func (e *Endpoint) GetCurrentRequests() int {
-	return e.currentRequests
+	return e.CurrentRequests
 }
 
 func (e *Endpoint) GetLastResponseTime() time.Duration {
-	return e.lastResponseTime
+	return e.LastResponseTime
 }
 
 func (e *Endpoint) GetURL() *url.URL {
-	return e.url
+	return e.Url
 }
