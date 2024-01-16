724979f810945b5e812fbb9193fd502857006066
Finn Wessel <finn.wessel@stud.hs-flensburg.de>
2024-01-16 14:48:05 +0100

    feat(pinia): implement state persistence

--- frontend/src/stores/auth-store.ts
+++ frontend/src/stores/auth-store.ts
@@ -1,18 +1,29 @@
-import {defineStore} from "pinia";
-import {Credentials} from "components/models";
-import AuthService from "src/service/AuthService";
+import {defineStore} from 'pinia';
+import {Credentials} from 'components/models';
+import AuthService from 'src/service/AuthService';
+import { api } from 'boot/axios'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     token: '',
     username: '',
   }),
+  persist: {
+    afterRestore: (ctx) => {
+      ctx.store.hydrate();
+    }
+  },
   getters: {
     isAuthenticated(): boolean {
       return this.token !== '';
     },
   },
   actions: {
+    hydrate() {
+      if (this.token) {
+        api.defaults.headers.common['Authorization'] = 'Bearer ' + this.token
+      }
+    },
     logout() {
       this.token = '';
       this.username = '';
@@ -20,6 +31,8 @@ export const useAuthStore = defineStore('auth', {
     async login(credentials: Credentials): Promise<void> {
       return new Promise((resolve, reject) => {
         AuthService.login(credentials).then((res) => {
+          api.defaults.headers.common['Authorization'] = 'Bearer ' + res.access_token
+          this.token = res.access_token;
           resolve();
         }).catch((err) => reject(err));
       });
@@ -31,5 +44,5 @@ export const useAuthStore = defineStore('auth', {
         }).catch((err) => reject(err));
       });
     }
-  }
+  },
 });
